7391d051157dfa45be2771721a0e7747309834fe
Viktor Yakubiv <viktor.yakubiv@gmail.com>
2017-09-12 01:14:30 +0300

    Improve model

--- app/model/index.js
+++ app/model/index.js
@@ -1,20 +1,20 @@
-import { REPO_ANY } from '../config';
-import { filter, sort } from './helpers';
+import GithubApi from './api/GithubApi';
+import Router from './router';
+import { REPO_ANY } from './constants';
+import * as data from './data';
 
 
-export default class GitHubClientModel {
-  constructor(render) {
+class GithubModel {
+  constructor(render, { apiUrl = '/', history = window.history } = {}) {
+    this.api = new GithubApi(apiUrl);
+    this.router = new Router(history);
     this.render = render;
 
     this.state = {
       owner: null,
-      repos: {
-        fetched: [],
-        next: null,
-
-        used: [],
-      },
-      dialog: null,
+      repos: [],
+      page: 0,
+      opened: null,
 
       filters: {
         type: REPO_ANY,
@@ -26,169 +26,107 @@ export default class GitHubClientModel {
         hasTopics: false,
       },
 
-      sort: null,
+      sort: {
+        param: null,
+        order: 1,
+      },
     };
-
-    this.present = this.present.bind(this);
   }
 
-  saveData(data) {
+  saveData({ owner, repos, filter, sort }) {
     const state = this.state;
     const updates = {};
 
-    if (data.owner) {
-      state.owner = data.owner || state.owner;
+    if (owner) {
+      state.owner = owner;
+      state.repos = [];
+      state.page = 0;
+
       updates.owner = true;
+    }
 
-      state.repos = {
-        fetched: [],
-        next: null,
+    if (repos) {
+      state.onwer.repos = state.onwer.repos.concat(repos);
+      state.requests.remove('repos');
 
-        used: [],
-      };
       updates.repos = true;
     }
 
-    if (data.repos) {
-      state.repos.fetched = state.repos.fetched
-        .concat(data.repos.map(repo => Object.assign(repo, {
-          updatedAt: new Date(repo.updatedAt),
-          createdAt: new Date(repo.createdAt),
-        })));
-      state.repos.next = data.next || null;
-      updates.repos = true;
-    }
+    if (filter) {
+      const { name, value } = filter;
 
-    if (data.repo && state.repos.fetched) {
-      const repo = state.repos.fetched
-        .find(({ fullName }) => (data.repo.fullName === fullName));
-
-      if (repo) {
-        Object.assign(repo, data.repo);
-
-        repo.updatedAt = new Date(repo.updatedAt);
-        repo.createdAt = new Date(repo.createdAt);
-
-        state.dialog = repo;
-        updates.dialog = true;
+      if (Object.prototype.hasOwnProperty.call(state.filters, name)) {
+        state.filters[name] = value;
+        updates.filters = true;
       }
     }
 
-    if (data.filters) {
-      const filters = state.filters;
+    if (sort) {
+      const { param, order } = sort;
 
-      Object.keys(data.filters).forEach((name) => {
-        if (Object.prototype.hasOwnProperty.call(filters, name)) {
-          filters[name] = data.filters[name];
-          updates.filters = true;
-        }
-      });
-    }
-
-    if (data.sort) {
-      state.sort = data.sort;
+      state.sort = { param, order };
       updates.sort = true;
     }
-
-    return updates;
   }
 
-  postUpdate(updates) {
-    const state = this.state;
+  syncWithServer(updates) {
+    const { api, state, present } = this;
 
-    if (updates.repos || updates.filters) {
-      const { repos, filters } = state;
+    if (updates.owner && (typeof state.owner === 'string')) {
+      const ownerName = state.owner;
 
-      repos.used = filter(repos.fetched, filters);
-    }
-
-    if (updates.repos || updates.filters || updates.sort) {
-      sort(state.repos.used, state.sort);
-    }
-  }
-
-  async present(data, render = this.render) {
-    if (process.env.NODE_ENV === 'development') {
-      console.log('%cPRESENT', 'color: green;', data);
-    }
-
-    if (!data) return;
-
-    const updates = this.saveData(data);
-    this.postUpdate(updates);
-
-    // TODO: Remove in case of pure usage of present method
-    // if (data.init) {
-    //   state = data.init;
-    //   updates.init = true;
-    // }
-
-    if (process.env.NODE_ENV === 'development') {
-      console.log('%cMODEL', 'color: green;', this);
-    }
-
-    render(this.state, this.present);
-  }
-}
-
-function present(data, render) {
-  const { api, state } = this;
-  const updates = {};
-
-  if (data.owner) {
-    if (typeof data.owner === 'string') {
-      api.fetchOwner(data.owner)
-        .then(owner => this.present({ owner }))
-        .catch(error => this.present({
+      api.fetchOwner(state.owner)
+        .then(owner => present({ owner }))
+        .catch(error => present({
           error,
           resource: 'owner',
-          request: data.owner,
+          request: ownerName,
         }));
 
+      state.owner = null;
+      state.repos = [];
+      state.page = 0;
       state.requests.add('owner');
-    } else {
-      state.owner = data.owner;
-      state.repos = {
-        fetched: [],
-        nextPage: 1,
+    }
 
-        used: [],
-      };
-      state.requests.remove('owner');
-      updates.owner = true;
-
+    if (updates.page) {
       api.fetchRepos(state.owner.login, state.owner)
-        .then(repos => this.present({ repos }))
-        .catch(error => this.present({
+        .then(repos => present({ repos }))
+        .catch(error => present({
           error,
           resource: 'repos',
-          request: data.owner.login,
+          request: state.owner.login,
         }));
 
       state.requests.add('repos');
     }
   }
 
-  if (data.repos) {
-    state.repos = data.repos;
-    state.repos.nextPage = data.repos.fetched.length < state.owner.publicRepos
-      ? state.repos.nextPage + 1
-      : null;
-    state.requests.remove('repos');
+  postUpdate(updates) {
+    const state = this.state;
 
-    updates.repos = true;
-  }
+    if (updates.repos || updates.filters) {
+      state.repos = data.filter(state.onwer.repos, state.filters);
+    }
 
-  if (data.filter) {
-    const { name, value } = data.filter;
-
-    if (Object.prototype.hasOwnProperty.call(state.filters, name)) {
-      state.filters[name] = value;
-      updates.filters = true;
+    if (updates.repos || updates.filters || updates.sort) {
+      data.sort(state.repos, state.sort);
     }
   }
 
-  if (data.sort) {
-    const { param, dir } = data.sort;
+  async present(message, render = this.render) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('%cPRESENT', 'color: green;', message);
+    }
+
+    const updates = this.saveData(message);
+    this.syncWithServer(updates);
+    this.postUpdate(updates);
+
+    if (process.env.NODE_ENV === 'development') {
+      console.log('%cMODEL', 'color: green;', this);
+    }
+
+    render(this.state, this.present);
   }
 }
